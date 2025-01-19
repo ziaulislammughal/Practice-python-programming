80a7bf8c9d14d456b812b399b16e38536e784e50
ziaulislammughal <learningwithzia@gmail.com>
2025-01-19 11:46:04 +0500

    learn about conditions

--- day2.py
+++ day2.py
@@ -31,3 +31,6 @@ for i in range(10):
     if i == 5:
         break  # stop the loop when i equals 5
     print(i) #output 0,1,2,3,4
+    
+    
+    
---
+++ day3.py
@@ -0,0 +1,11 @@
+# learning about concidtions if elif else . 
+# structure
+
+age = int(input("Enter age :"))
+if age<18:
+    print("you are not eligible for driving.")
+elif age>18 and age<65:
+    print("you are eligible for driving.")
+else:
+    print("you are just a teenager.")
+    
