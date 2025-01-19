6f498cd1dc0b2c568928bd3a7449ea1f484682ca
ziaulislammughal <learningwithzia@gmail.com>
2025-01-19 11:37:59 +0500

    learn about break and continue in loops

--- day2.py
+++ day2.py
@@ -1,20 +1,33 @@
-# LOOPS .
+#  LOOPS .
 # FOR loop 
-# # WHILE LOOP 
+ # WHILE LOOP 
 
-# for i in range(1,11,3):  #index start from 0 to 9            range(start,stop,step)
- #     print(i)          #output 0,1,2,3,4,5,6,7,8,9
+for i in range(1,11,3):  #index start from 0 to 9            range(start,stop,step)
+ print(i)          #output 0,1,2,3,4,5,6,7,8,9
     
     
-# fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
-# for i in fruits:
-#     print(i)       # output apple, banana, cherry, date, elderberry
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+for fruit in fruits:
+     print(i)       # output apple, banana, cherry, date, elderberry
 
 
-# # while loop
+# while loop
 
 count=1 
 while count<1000:
     print(count)
     count += 1      # increment count by 1 each time
     count = count**2   # increment count by 1 each time
+    
+    
+# break and continue statement . 
+for i in range(10):
+    if i == 5:
+        continue  # stop the loop when i equals 5
+    print(i)  #output 0,1,2,3,4, we use continue os iteration is print 5 and move on ,6,7,8,9
+    
+    
+for i in range(10):
+    if i == 5:
+        break  # stop the loop when i equals 5
+    print(i) #output 0,1,2,3,4
