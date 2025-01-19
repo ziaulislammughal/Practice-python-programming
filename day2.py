94e1cb0e559783180d6cf0304020a24f1f450e59
ziaulislammughal <learningwithzia@gmail.com>
2025-01-19 11:28:36 +0500

    learn basic about for and while loop

---
+++ day2.py
@@ -0,0 +1,20 @@
+# LOOPS .
+# FOR loop 
+# # WHILE LOOP 
+
+# for i in range(1,11,3):  #index start from 0 to 9            range(start,stop,step)
+ #     print(i)          #output 0,1,2,3,4,5,6,7,8,9
+    
+    
+# fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+# for i in fruits:
+#     print(i)       # output apple, banana, cherry, date, elderberry
+
+
+# # while loop
+
+count=1 
+while count<1000:
+    print(count)
+    count += 1      # increment count by 1 each time
+    count = count**2   # increment count by 1 each time
