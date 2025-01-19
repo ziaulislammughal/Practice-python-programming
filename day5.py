997652a6a729f36b02b6066898b74525469a9c51
ziaulislammughal <learningwithzia@gmail.com>
2025-01-19 18:51:03 +0500

    using split function on string

--- day5.py
+++ day5.py
@@ -1,10 +1,3 @@
-sentence = input("Enter a sentence :\n")
-sentence = sentence.lower()
-vowels = "aeiou"
-vowels_count = 0 
-
-for char in sentence:
-    if char in vowels:
-        vowels_count += 1 
-
-print(f"The number of vowels in sentence is : {vowels_count}")
+txt = "apple#banana#cherry#orange"
+x = txt.split("#", maxsplit=1)
+print(x)
