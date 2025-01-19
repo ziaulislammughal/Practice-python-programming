b2150b7da6947b1eddc58987996d7f4be25b4ddf
ziaulislammughal <learningwithzia@gmail.com>
2025-01-19 12:05:12 +0500

    learn about operators eg comparison operator and logical operator

--- day3.py
+++ day3.py
@@ -1,11 +1,30 @@
 # learning about concidtions if elif else . 
 # structure
 
-age = int(input("Enter age :"))
-if age<18:
-    print("you are not eligible for driving.")
-elif age>18 and age<65:
-    print("you are eligible for driving.")
-else:
-    print("you are just a teenager.")
+# age = int(input("Enter age :"))
+# if age<18:
+#     print("you are not eligible for driving.")
+# elif age>18 and age<65:
+#     print("you are eligible for driving.")
+# else:
+#     print("you are just a teenager.")
     
+    
+    
+# operators conditions 
+# comparison operator and logical operator 
+
+# == equal to  x == y  return true if x is equal to y 
+# != not equal to  x != y  return true if x is not equal to y
+
+
+# logic operator 
+#  and or not . 
+
+
+number = int(input("Enter number :"))
+if number == 10 or  number > 25:
+    print("good choice")
+else:
+    print("not good choice ")
+
