334d7c4ec54f7e687806346ea588cf5864d3270d
ziaulislammughal <learningwithzia@gmail.com>
2025-01-26 18:03:37 +0500

    add some thing new

--- day5.py
+++ day5.py
@@ -1,3 +1,17 @@
-txt = "apple#banana#cherry#orange"
-x = txt.split("#", maxsplit=1)
-print(x)
+# Match case 
+
+number = 10 
+
+match number:
+    case 10:
+        print("The number is 10")
+    case 20:
+        print("The number is 20")
+    case var x where x.is_prime():
+        print(f"The number {x} is a prime number")
+    case var x where x.is_even():;
+        print(f"The number {x} is even")
+    case var x where x.is_odd():
+        print(f"The number {x} is odd")
+    case var x:
+        print(f"The number {x} is neither prime, even, nor odd")
